f113a9ce323cc20e6d0b4954e4804c1823547611
essraaamr2002 <77300591+essraaamr2002@users.noreply.github.com>
2023-04-23 10:31:08 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const sequelize = require('./util/database');
+const User = require('./models/user');
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+//test route
+app.get('/', (req, res, next) => {
+  res.send('Hello World');
+});
+
+//CRUD routes
+app.use('/users', require('./routes/users'));
+
+//error handling
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+//kafka 
+const client =new kafka.KafkaClient({kafkaHost: process.env.KKAFKA_BOOTSTRAP_SERVERS})
+const consumer =new kafka.Consumer(client,[{topic:process.env.KAFKA_TOPIC}],
+  {
+  autoCommit:false
+
+})
+consumer.on('message',async(message)=>{
+  const user =await new   User(JSON.parse(message.value))
+  await user.save()
+})
+
+//sync database
+sequelize
+  .sync()
+  .then(result => {
+    console.log("Database connected");
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
